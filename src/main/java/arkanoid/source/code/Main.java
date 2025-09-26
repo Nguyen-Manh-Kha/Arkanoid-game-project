70e276551bb36dc5c8790a5d7a299d00468cd867
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-09-26 17:08:44 +0700

    Update Main.java

--- src/main/java/arkanoid/source/code/Main.java
+++ src/main/java/arkanoid/source/code/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
         StackPane root = new StackPane(canvas);
         Scene scene = new Scene(root, 600, 400, Color.LIGHTGRAY);
 
-        stage.setTitle("Arkanoid heheheehhehee");
+        stage.setTitle("Arkanoid says hello");
         stage.setScene(scene);
         stage.show();
     }
@@ -37,3 +37,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+
